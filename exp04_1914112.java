1d983c48440a7abf35028037db3bdf762e555de6
ramavtarofficial <ramavtar.y@somaiya.edu>
2022-11-16 01:40:03 +0530

    Initial Commit

---
+++ exp04_1914112.java
@@ -0,0 +1,10 @@
+public class exp04_1914112 {
+    public static void main(String[] args) {
+           int first = 15;
+           int second = 10;
+           System.out.println("The First Number is = "+ first);
+           System.out.println("The Second Number is = "+ second);
+           System.out.println("The subtraction of number is = "+ (first - second));
+ 
+    }
+}
